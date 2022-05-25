348cd550a7cbd0fe0cbaace94dc9654010f57ace
Gerald Gonzales <104032271+Code-GeraldGonzales@users.noreply.github.com>
2022-05-25 12:20:07 -0700

    Add files via upload

---
+++ Gerald_Gonzales_Likes_Card/script.js
@@ -0,0 +1,23 @@
+console.log("connected to js");
+
+
+var count1 = 3;
+var counter1 = document.querySelector('#numberoflikes1');
+
+function add1() {
+    count1++;
+    counter1.innerText = count1 + " like(s)";
+}
+
+
+
+/*
+var clicks = 3;
+
+function onClick() {
+    clicks += 1;
+    document.getElementById("clicks").innerHTML = clicks + " like(s)";
+};
+
+*/
+
